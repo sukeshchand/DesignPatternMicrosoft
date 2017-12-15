9a04ecd5b895d176371343a75249f8d9625730b3
sukeshchand <sukesh.chand@bridge-india.in>
2017-12-15 23:08:55 +0530

    more comments added in factory pattern

--- Factory Method Design pattern/Realworld/Main.cs
+++ Factory Method Design pattern/Realworld/Main.cs
@@ -8,25 +8,7 @@ namespace BridgeTechWhizz
     {
         static void Main()
         {
-            Console.WriteLine("Create Resume for Higher Education (Creation is happening inside the concrete object)\n------------------------");
-            Document_Abstract resumeEducation1 = new Document_HigherEducationResume();
-            foreach (var resumeEducationPage in resumeEducation1.Pages)
-            {
-                Console.WriteLine(resumeEducationPage.PageContent);
-            }
-
-            Console.WriteLine("\n\n");
-            Console.WriteLine("Create Resume for Job (Creation is happening inside the concrete object)\n------------------------");
-            Document_Abstract resumeJob1 = new Document_JobResume();
-            foreach (var resumeJobPage in resumeJob1.Pages)
-            {
-                Console.WriteLine(resumeJobPage.PageContent);
-            }
-
-            Console.WriteLine("\n\n\n\n");
-
-
-            Console.WriteLine("Create Resume for Higher Education (Creation is happening in a separate factory class)\n------------------------");
+            Console.WriteLine("Create Resume for Higher Education (Creation of *Higher Education Resume* is happening in a separate factory class)\n------------------------");
             Document_Abstract resumeEducation2 = new DocumentFactory().GetDocument(1);
             foreach (var resumeEducationPage in resumeEducation2.Pages)
             {
@@ -34,14 +16,30 @@ namespace BridgeTechWhizz
             }
 
             Console.WriteLine("\n\n");
-            Console.WriteLine("Create Resume for Job (Creation is happening in a separate factory class)\n------------------------");
+            Console.WriteLine("Create Resume for Job (Creation of *Job Resume* is happening in a separate factory class)\n------------------------");
             Document_Abstract resumeJob2 = new DocumentFactory().GetDocument(2);
             foreach (var resumeJobPage in resumeJob2.Pages)
             {
                 Console.WriteLine(resumeJobPage.PageContent);
             }
+            
+            Console.WriteLine("\n\n\n\n");
 
+            Console.WriteLine("Create Resume for Higher Education (Creation of *Document pages of Higher Education Resume* is happening inside the concrete object)\n------------------------");
+            Document_Abstract resumeEducation1 = new Document_HigherEducationResume();
+            foreach (var resumeEducationPage in resumeEducation1.Pages)
+            {
+                Console.WriteLine(resumeEducationPage.PageContent);
+            }
 
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Create Resume for Job (Creation of *Document pages of Job Resume* is happening inside the concrete object)\n------------------------");
+            Document_Abstract resumeJob1 = new Document_JobResume();
+            foreach (var resumeJobPage in resumeJob1.Pages)
+            {
+                Console.WriteLine(resumeJobPage.PageContent);
+            }
+            
             Console.ReadKey();
         }
     }
---
+++ Factory Method Design pattern/Realworld/Readme.txt
@@ -0,0 +1,19 @@
+﻿Factory method pattern
+--------------------------------
+In class-based programming, the factory method pattern is a creational pattern that uses factory methods to deal with the problem of creating objects without having to specify the exact class of the object that will be created. 
+This is done by creating objects by 1)calling a factory method—either specified in an interface and implemented by child classes, or implemented in a base class and optionally overridden by derived classes—rather than by calling a constructor.
+
+Example
+--------
+Here we are trying to create two type of resumes. 
+1) Resume for Higher education
+2) Resume for Job
+
+A factory method called DocumentFactory will be taken care of creating the object of the above
+Document_Abstract is the abstract class which has a method called CreatePages(), The creation of pages in the document will be taken care by the CreatePages()
+
+Points to note
+------------------
+The Document_HigherEducationResume and Document_JobResume are implemented from Document_Abstract so we can point/store the two pages in to Document_Abstract
+Page_Education, Page_Experience, Page_PersonalDetails and Page_Skills is a derived from Page_Abstract, So we can create object of any one of the page and store/point towards the Page_Abstract
+
